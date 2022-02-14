85d2901b86c0bc677c98daac42f88807bebdd4a8
wangqing <faithlocus86@gmail.com>
2022-02-14 10:56:56 +0800

    decoder init/start

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -1,1 +1,43 @@
 #include "decoder.h"
+
+
+void decoder_init(Decoder *d,
+                  AVCodecContext *avctx,
+                  PacketQueue *queue,
+                  SDL_cond *empty_queue_cond){
+    memset(d, 0, sizeof(Decoder));
+    d->pkt = av_packet_alloc();
+    if(!d->pkt)
+        return AVERROR(ENOMEM);
+    d->avctx = avctx;
+    d->queue = queue;
+    d->empty_queue_cond = empty_queue_cond;
+    d->start_pts        = AV_NOPTS_AVLUE;
+    d->pkt_serial       = -1;
+    return 0;
+}
+
+int decoder_start(Decoder *d,
+                  int (*fc)(void *),
+                  const char *thread_name,
+                  void *arg){
+    packet_queue_start(d->queue);
+    d->decoder_tid = SDL_CreateThread(fn, thread_name, arg);
+    if (!d->decoder_tid){
+        elog("SDL_CreateThread():%s\n", SDL_GetError());
+        return AVERROR(ENOMEM);
+    }
+    return 0;
+}
+
+int audio_thread(void *arg){
+
+}
+
+int video_thread(void *arg){
+
+}
+
+int subtitle_thread(void *arg){
+
+}
--- src/decoder.h
+++ src/decoder.h
@@ -15,20 +15,20 @@ extern "C" {
 struct PacketQueue;
 // 解码器，音频，视频，字幕独立不影响,即独立线程解码
 struct Decoder {
-    AVPacket        pkt;
-    PacketQueue*    queue;
-    AVCodecContext* avctx;
+    AVPacket        pkt; 
+    PacketQueue*    queue;          // 数据包队列
+    AVCodecContext* avctx;          // 解码上下文
 
-    int       pkt_serial;
-    int       finished;
-    int       packet_pending;
-    SDL_cond* empty_queue_cond;
+    int       pkt_serial;           // 包序列
+    int       finished;             // =0工作状态， !=0空闲状态
+    int       packet_pending;       // =0解码器异常，需要重置，=1解码器处于正常状态
+    SDL_cond* empty_queue_cond;     // 检测到packet队列空时发送该信号通知read_thread读取数据
 
-    int64_t     start_pts;
-    AVRational  start_pts_tb;
-    int64_t     next_pts;
-    AVRational  next_pts_tb;
-    SDL_Thread* decoder_tid;
+    int64_t     start_pts;          // 初始化时stream的start_time
+    AVRational  start_pts_tb;       // 初始化时stream的time_base
+    int64_t     next_pts;           // 记录最近一次解码后的frame的pts，当解出来的部分帧没有有效的pts时，使用该变量进行推算
+    AVRational  next_pts_tb;        // next_pts的单位
+    SDL_Thread* decoder_tid;        // 线程句柄
 };
 
 int audio_thread(void* arg);
