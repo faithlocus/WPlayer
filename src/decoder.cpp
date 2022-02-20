4ae39d2abf47a610a3ba508cad90ea363102dae6
fl <wangqing10520@sina.com>
2022-02-20 15:27:29 +0800

    subtitle thread

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -340,8 +340,37 @@ the_end:
     return 0;
 }
 
-int subtitle_thread(void *arg) {}
+int subtitle_thread(void *arg) {
+    MainState* is = arg;
+    Frame*     sp;
+    int        got_subtitle;
+    double     pts;
 
+    while(true){
+        if (!(sp = frame_queue_peek_writable(&is->subpq)))
+            return 0;
+
+        if (got_subtitle = decoder_decoder_frame(&is->subdec, NULL, &sp->sub) < 0)
+            break;
+
+        pts = 0;
+
+        if (got_subtitle && sp->sub->format == 0){
+            if (sp->sub.pts != AV_NOPTS_VALUE)
+                pts = sp->sub.pts / ( double )AV_TIME_BASE;
+            sp->pts = pts;
+            sp->serial = is->subdec.pkt_serial;
+            sp->width  = is->subdec.avctx->width;
+            sp->height = is->subdec.avctx->height;
+            sp->uploaded = 0;
+
+            frame_queue_push(&is->subpq);
+        }else if (got_subtitle){
+            avsubtitile_free(&sp->sub);
+        }
+    }
+    return 0;
+}
 
 static int get_video_frame(MainState *is, AVFrame *frame){
 
